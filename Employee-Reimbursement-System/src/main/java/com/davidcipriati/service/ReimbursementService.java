97bdb4973913860030e433fd4a8b6a04366f3b8d
David Cipriati <devcipriati@gmail.com>
2022-07-28 17:28:08 -0400

    Add services

--- Employee-Reimbursement-System/src/main/java/com/davidcipriati/service/ReimbursementService.java
+++ Employee-Reimbursement-System/src/main/java/com/davidcipriati/service/ReimbursementService.java
@@ -1,2 +1,23 @@
-package com.davidcipriati.service;public class ReimbursementService {
+package com.davidcipriati.service;
+
+import com.davidcipriati.model.Reimbursement;
+import com.davidcipriati.repository.ReimbursementDAO;
+
+import java.util.List;
+
+public class ReimbursementService {
+    private ReimbursementDAO reimbursementDAO;
+
+    public ReimbursementService(ReimbursementDAO reimbursementDAOImpl) {
+        this.reimbursementDAO = reimbursementDAOImpl;
+    }
+
+    public List<Reimbursement> getAllPendingReimbursements() {
+        return reimbursementDAO.findAllPending();
+    }
+
+    public List<Reimbursement> getAllPendingByUserId(int userId) {
+        return reimbursementDAO.findAllByUserId(userId);
+    }
+
 }
--- Employee-Reimbursement-System/src/main/java/com/davidcipriati/service/UserService.java
+++ Employee-Reimbursement-System/src/main/java/com/davidcipriati/service/UserService.java
@@ -1,2 +1,19 @@
-package com.davidcipriati.service;public class UserService {
+package com.davidcipriati.service;
+
+import com.davidcipriati.model.User;
+import com.davidcipriati.repository.UserDAO;
+import com.davidcipriati.repository.UserDAOImpl;
+
+import java.util.List;
+
+public class UserService {
+    private UserDAO userDAO;
+
+    public UserService(UserDAO userDAOImpl) {
+        this.userDAO = userDAOImpl;
+    }
+
+    public List<User> getAllEmployees() {
+        return userDAO.findAllUsers();
+    }
 }
